1140393550b4492e23a54d2093949dbccfd06ef9
Paulo Vianna <paulovianna.lwk@gmail.com>
2020-12-05 16:32:53 -0300

    Building script to drag

---
+++ TowerDefense/Assets/Scripts/Building.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Building : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    private Vector3 offset;
+    private float yPosition;
+    private bool selected;
+
+    private void Start()
+    {
+        yPosition = transform.position.y;
+    }
+
+    public void OnPointerDown(PointerEventData pointerEventData)
+    {
+        selected = true;
+        offset = transform.position - pointerEventData.pointerCurrentRaycast.worldPosition;
+    }
+
+    public void OnPointerUp(PointerEventData pointerEventData)
+    {
+        selected = false;
+    }
+
+    public void OnDrag(PointerEventData pointerEventData)
+    {
+        var newPosition = pointerEventData.pointerCurrentRaycast.worldPosition + offset;
+        transform.position = new Vector3(newPosition.x, yPosition, newPosition.z);
+    }
+}
