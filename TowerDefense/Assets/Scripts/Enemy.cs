d2eec748460dbda007d83283818d19131695548c
Paulo Vianna <paulovianna.lwk@gmail.com>
2020-12-05 09:21:03 -0300

    Arena Scene with NavMesh

---
+++ TowerDefense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private NavMeshAgent navMeshAgent;
+
+    private void Start()
+    {
+        navMeshAgent.SetDestination(targetTransform.position);
+    }
+}
