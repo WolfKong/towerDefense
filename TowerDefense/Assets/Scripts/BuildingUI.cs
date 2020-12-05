2e2bff07760d609869b25f40a042afe9631f5fa9
Paulo Vianna <paulovianna.lwk@gmail.com>
2020-12-05 16:32:05 -0300

    BuildingUI prefab

---
+++ TowerDefense/Assets/Scripts/BuildingUI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingUI : MonoBehaviour
+{
+    [SerializeField] private Button confirmButton;
+    [SerializeField] private Button moveButton;
+    [SerializeField] private Button destroyButton;
+
+    public static event Action ConfirmEvent;
+    public static event Action MoveEvent;
+    public static event Action DestroyEvent;
+
+    void Start()
+    {
+        confirmButton.onClick.AddListener(() => ConfirmEvent?.Invoke());
+        moveButton.onClick.AddListener(() => MoveEvent?.Invoke());
+        destroyButton.onClick.AddListener(() => DestroyEvent?.Invoke());
+    }
+}
