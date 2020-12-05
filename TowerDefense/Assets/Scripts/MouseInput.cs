70dc7d96729f90b744940ecb346b4728530ed258
Paulo Vianna <paulovianna.lwk@gmail.com>
2020-12-05 14:48:55 -0300

    MouseInput and WallSpawner

---
+++ TowerDefense/Assets/Scripts/MouseInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseInput : MonoBehaviour, IPointerClickHandler
+{
+    public static event Action<Vector3> Clicked;
+
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        Clicked?.Invoke(pointerEventData.pointerCurrentRaycast.worldPosition);
+    }
+}
---
+++ TowerDefense/Assets/Scripts/WallSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class WallSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform wallPrefab;
+
+
+    private void Start()
+    {
+        MouseInput.Clicked += Spawn;
+    }
+
+    private void OnDestroy()
+    {
+        MouseInput.Clicked -= Spawn;
+    }
+
+    private void Spawn(Vector3 position)
+    {
+        var wall = Instantiate(wallPrefab, transform);
+        var wallTransform = wall.transform;
+        wallTransform.position = new Vector3(position.x, position.y + wallTransform.localScale.y * 0.5f, position.z);
+    }
+}
