a7d231a4b86ad5ce3695d116992070227211a439
Paulo Vianna <paulovianna.lwk@gmail.com>
2020-12-06 11:38:20 -0300

    GoalHP FloatVariable

---
+++ TowerDefense/Assets/Scripts/ScriptableObjects/FloatVariable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "FloatVariable", menuName = "ScriptableObjects/FloatVariable")]
+public class FloatVariable : ScriptableObject
+{
+    public float Value;
+
+    public override string ToString()
+    {
+        return $"Value:{Value}";
+    }
+}
