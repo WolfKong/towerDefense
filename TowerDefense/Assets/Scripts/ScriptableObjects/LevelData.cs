7b1bf404bab4c7a591c94db127a9818de8ff1408
Paulo Vianna <paulovianna.lwk@gmail.com>
2020-12-06 14:40:46 -0300

    LevelData

---
+++ TowerDefense/Assets/Scripts/ScriptableObjects/LevelData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelData", menuName = "ScriptableObjects/LevelData")]
+[Serializable]
+public class LevelData : ScriptableObject
+{
+    public int GoalHealth;
+
+    public override string ToString()
+    {
+        return $"GoalHealth:{GoalHealth}";
+    }
+}
