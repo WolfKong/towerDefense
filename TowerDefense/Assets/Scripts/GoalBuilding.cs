87335ce46727f27f564f6a2d95fdf9a899046d84
Paulo Vianna <paulovianna.lwk@gmail.com>
2020-12-06 11:44:09 -0300

    GoalBuilding prefab and script

---
+++ TowerDefense/Assets/Scripts/GoalBuilding.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GoalBuilding : MonoBehaviour
+{
+    [SerializeField] private FloatVariable health;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Enemy")
+        {
+            health.Value--;
+        }
+    }
+}
