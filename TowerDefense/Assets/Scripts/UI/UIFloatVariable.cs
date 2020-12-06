48aab75945fd1c6f05a464826b9b62373479193b
Paulo Vianna <paulovianna.lwk@gmail.com>
2020-12-06 11:54:20 -0300

    UIFloatVariable for GoalHP

---
+++ TowerDefense/Assets/Scripts/UI/UIFloatVariable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFloatVariable : MonoBehaviour
+{
+    [SerializeField] private Text uiText;
+    [SerializeField] private FloatVariable floatVariable;
+    [SerializeField] private string prefix;
+
+    private float value;
+
+    private void Update()
+    {
+        if (value != floatVariable.Value)
+        {
+            value = floatVariable.Value;
+            uiText.text = $"{prefix}{value}";
+        }
+    }
+}
