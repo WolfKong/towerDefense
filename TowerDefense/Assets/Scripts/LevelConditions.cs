fa792841d6ee8a27fbd99c1c00f95489ad8658ef
Paulo Vianna <paulovianna.lwk@gmail.com>
2020-12-06 14:45:42 -0300

    LevelConditions for game over

---
+++ TowerDefense/Assets/Scripts/LevelConditions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class LevelConditions : MonoBehaviour
+{
+    [SerializeField] private FloatVariable goalBuildingHealth;
+    [SerializeField] private LevelData levelData;
+
+    private bool gameOver;
+
+    void Start()
+    {
+        gameOver = false;
+        goalBuildingHealth.Value = levelData.GoalHealth;
+    }
+
+    private void Update()
+    {
+        if (!gameOver && goalBuildingHealth.Value <= 0)
+        {
+            gameOver = true;
+            SceneLoader.LoadScene("GameOver");
+        }
+    }
+}
