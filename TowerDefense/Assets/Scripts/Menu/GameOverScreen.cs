9eef416ac685e2438d2ff4d249d7403d66ee8f46
Paulo Vianna <paulovianna.lwk@gmail.com>
2020-12-06 14:41:50 -0300

    GameOver scene

---
+++ TowerDefense/Assets/Scripts/Menu/GameOverScreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScreen : MonoBehaviour
+{
+    [SerializeField] private Text wavesLeftText;
+    [SerializeField] private FloatVariable waves;
+    [SerializeField] private Button retryButton;
+
+    void Start()
+    {
+        wavesLeftText.text = $"{waves.Value} waves left";
+        retryButton.onClick.AddListener(RetryLevel);
+    }
+
+    private void RetryLevel()
+    {
+        SceneLoader.LoadScene("Arena");
+    }
+}
