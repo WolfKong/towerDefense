80c45193f00e8980205caec54820f3d66607b974
Paulo Vianna <paulovianna.lwk@gmail.com>
2020-12-06 12:26:34 -0300

    SceneLoader and Transition

---
+++ TowerDefense/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private static SceneLoader instance;
+
+    [SerializeField] private Animator animatorPrefab;
+    private Animator animator;
+
+    private void Awake()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+        animator = Instantiate(animatorPrefab, transform);
+    }
+
+    public static void LoadScene(string sceneName)
+    {
+        if (instance == null)
+        {
+            SceneManager.LoadScene(sceneName);
+            return;
+        }
+
+        instance.AnimatedLoadScene(sceneName);
+    }
+
+    private void AnimatedLoadScene(string sceneName)
+    {
+        StartCoroutine(LoadSceneCoroutine(sceneName));
+    }
+
+    private IEnumerator LoadSceneCoroutine(string sceneName)
+    {
+        animator.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1f);
+
+        SceneManager.LoadScene(sceneName);
+
+        animator.SetTrigger("End");
+    }
+}
