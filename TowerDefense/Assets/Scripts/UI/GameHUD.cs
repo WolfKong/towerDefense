2e6270fc0fee547e4b69416402c6d2d646ca490c
Paulo Vianna <paulovianna.lwk@gmail.com>
2020-12-09 20:02:04 -0300

    Wave number on Game HUD

---
+++ TowerDefense/Assets/Scripts/UI/GameHUD.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameHUD : MonoBehaviour
+{
+    [SerializeField] private Text wavesText;
+    [SerializeField] private LevelData levelData;
+
+    private int currentWave = -1;
+
+    private void Update()
+    {
+        if (levelData.CurrentWave != currentWave)
+        {
+            currentWave = levelData.CurrentWave;
+            wavesText.text = $"Wave {currentWave + 1}";
+        }
+    }
+}
