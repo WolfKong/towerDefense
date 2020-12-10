0d8a8481f79751431e4f43d72ae1671cd6e87f5d
Paulo Vianna <paulovianna.lwk@gmail.com>
2020-12-10 18:13:48 -0300

    RuntimeList BuildingList

---
+++ TowerDefense/Assets/Scripts/ScriptableObjects/RuntimeList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "List", menuName = "ScriptableObjects/RuntimeList")]
+[Serializable]
+public class RuntimeList : ScriptableObject
+{
+    public List<GameObject> Items;
+
+    public override string ToString()
+    {
+        return $"RuntimeList - Items\n{string.Join(",\n", Items)}";
+    }
+}
